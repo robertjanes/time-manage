25be3144338499b2c3e401466dfc67e150ff9d0d
robertjanes <robertjanes2@gmail.com>
2016-10-12 00:29:09 +1100

    Updated with setEvent, deleteEvent Created Event id generation

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
 'use strict';
 
-class TimeManager {
+class TimeManage {
   constructor(int, debug) {
-    this.time;
+    this.date;
     this.interval = int;
     this.callbacks = [];
+    this.events = [];
     this.debug = debug;
   }
 
+  getEventIdTrial(date) {
+    return date.getTime().toString() + Date.now().toString() + this.events.length.toString();
+  }
+
+  getAttachIdTrial() {
+    return Date.now().toString() + this.callbacks.length.toString();
+  }
+
+  setEvent(date, callback) {
+    let newEvent = {id: this.getEventIdTrial(date), date, callback};
+    this.events.push(newEvent);
+    if (this.debug) console.log('Event with id ' + newEvent.id + ' set for ' + newEvent.date);
+    return newEvent.id;
+  }
+
+  deleteEvent(id) {
+    for (let e = 0; e < this.events.length; e += 1) {
+      if (id === this.events[e].id) {
+        this.events.splice(e, 1);
+        if (this.debug) console.log('Event with id ' + id + ' deleted');
+      }
+    }
+  }
+
+  callSetEvents() {
+    for (let e = this.events.length - 1; e >= 0; e -= 1) {
+      if (this.date > this.events[e].date.getTime()) {
+        this.events[e].callback();
+        this.events.splice(e, 1);
+      }
+    }
+  }
+
   update() {
-    this.time = Date.now();
-    this.callAttached(this.time);
+    this.date = Date.now();
+    this.callAttached(this.date);
+    this.callSetEvents();
     let nextInterval = this.calcDrift() * 1000;
     if (this.debug) console.log('Next interval: ' + (nextInterval / 1000) + 's');
     setTimeout(this.update.bind(this), nextInterval);
@@ -23,22 +58,30 @@ class TimeManager {
     return drift;
   }
 
-  get() {
-    return this.time;
+  getTime() {
+    return this.date;
   }
 
-  attach(func) {
-    this.callbacks.push(func);
-    return this.callbacks.length - 1;
+  attach(callback) {
+    let attachEvent = {id: this.getAttachIdTrial(), callback}
+    this.callbacks.push(attachEvent);
+    if (this.debug) console.log('Attach Event with id ' + attachEvent.id + ' created');
+    return attachEvent.id;
   }
 
   detach(id) {
-    this.callbacks[id] = false;
+    for (let c = 0; c < this.callbacks.length; c += 1) {
+      if (id === this.callbacks[c].id) {
+        this.callbacks.splice(c, 1);
+        if (this.debug) console.log('Attach Event with id ' + id + ' deleted');
+      }
+    }
   }
 
   callAttached(time) {
     for (let c = 0; c < this.callbacks.length; c += 1) {
-      if (this.callbacks[c]) this.callbacks[c](time);
+      this.callbacks[c].callback(time);
+      if (this.debug) console.log('Callback for attach Event with id ' + this.callbacks[c].id + ' called');
     }
   }
 
@@ -47,4 +90,4 @@ class TimeManager {
   }
 }
 
-module.exports = TimeManager;
+module.exports = TimeManage;
--- test/test.js
+++ test/test.js
@@ -1,3 +1,22 @@
+'use strict';
 const timeManager = require(__dirname + '/../index.js');
 const tm = new timeManager(60000, true);
+
+tm.attach(function() {
+  console.log('Attached function working.');
+});
+
+let thisEvent = tm.setEvent(new Date(2016, 9, 12, 0, 27, 0), function() {
+  console.log('SetEvent function working.');
+});
+let thisEvent2 = tm.setEvent(new Date(2016, 9, 11, 23, 42, 0), function() {
+  console.log('SetEvent function working.');
+});
+let thisEvent3 = tm.setEvent(new Date(2016, 9, 11, 23, 42, 0), function() {
+  console.log('SetEvent function working.');
+});
+//tm.deleteEvent(thisEvent);
+tm.deleteEvent(thisEvent2);
+tm.deleteEvent(thisEvent3);
+
 tm.init();
